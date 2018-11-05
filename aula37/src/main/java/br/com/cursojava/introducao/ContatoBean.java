4ef08680ae4ef4883fd27a2d2247cbc0f7a4852d
gilliard Milliorini <gilliardmilliorini123@gmail.com>
2018-11-05 17:24:33 -0200

    Continuação de Manuseio JSF

---
+++ aula37/src/main/java/br/com/cursojava/introducao/ContatoBean.java
@@ -0,0 +1,68 @@
+package br.com.cursojava.introducao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@ViewScoped				// utilizado para mandar os dados somente enquanto a pagina nao for atualizada, perde os dados quando atualiza a page
+public class ContatoBean {
+	
+	private String nome;
+	private String email;
+	private String telefone;
+	private List<Contato> contatos = new ArrayList<>();
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public List<Contato> getContatos() {
+		return contatos;
+	}
+	public void setContatos(List<Contato> contatos) {
+		this.contatos = contatos;
+	}
+	
+	public void salvar(){
+		
+		Contato contato = new Contato(nome,email, telefone);
+		contatos.add(contato);
+		limparCampos();
+		FacesContext contex = FacesContext.getCurrentInstance();
+		FacesMessage mensagem = new FacesMessage("Contato Adicionado com sucesso");
+		mensagem.setSeverity(FacesMessage.SEVERITY_INFO);
+		contex.addMessage("nome", mensagem);
+		
+	}
+	
+	private void limparCampos(){
+		this.nome = "";
+		this.email = "";
+		this.telefone = "";
+	}
+	
+	
+	
+
+}
--- aula37/src/main/java/br/com/cursojava/introducao/HelloWorldBean2.java
+++ aula37/src/main/java/br/com/cursojava/introducao/HelloWorldBean2.java
@@ -1,41 +1,40 @@
 package br.com.cursojava.introducao;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-
-@ManagedBean(name="helloWorldBean")
+@ManagedBean(name="helloWorldBean2")
 @SessionScoped
-public class HelloWorldBean {
+public class HelloWorldBean2 {
 
-
+	
 	private String nome;
-	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	
+	private String time;
+	
+	
+	
 	
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
-	public String getDataHora(){
-		return sdf.format(new Date());
+	public String getTime() {
+		return time;
 	}
-	
+	public void setTime(String time) {
+		this.time = time;
+	}
 	
 	public String salvar(){					// criando uma validação para que o nome não seja nulo e tenha 3 ou mais caracteres
 		if(this.nome != null && this.nome.length() >= 3){
-			return "result";
+			return "resultEx";
 		}else {
 			return null;
 		}
 	}
-}
 
 	
-	
+}
